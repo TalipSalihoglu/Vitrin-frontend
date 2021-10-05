2fbead590d080091c313db083913beb7a51c52a4
TalipSalihoglu <59333488+TalipSalihoglu@users.noreply.github.com>
2021-10-05 18:18:44 +0300

    added models and VitrinAPI

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Product } from 'src/app/models/product';
+import { HttpClient } from '@angular/common/http'
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -7,16 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  product1 = { productId: 1, productName: "bardak", categoryId: 1, unitPrice: 95 }
-  product2 = { productId: 2, productName: "bıçak", categoryId: 2, unitPrice: 76 }
-  product3 = { productId: 3, productName: "tabak", categoryId: 3, unitPrice: 57 }
-  product4 = { productId: 4, productName: "çatal", categoryId: 4, unitPrice: 38 }
-  product5 = { productId: 5, productName: "kaşık", categoryId: 5, unitPrice: 19 }
-
-  products = [this.product1, this.product2, this.product3, this.product4, this.product5]
-  constructor() { }
+  products:Product[] = [];
+  apiUrl="https://localhost:5001/api/products/getall"
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+    this.getProudcts()
+  }
+
+  getProudcts(){
+    this.httpClient.get<ProductResponseModel>(this.apiUrl).subscribe((response)=>{
+      this.products=response.data
+    })
   }
 
 }
